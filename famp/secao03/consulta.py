ab0588218a9eaf0714a7de8574ea5e58e5873d5d
Alison Ghedin <alisonjghedin@gmail.com>
2022-06-17 15:28:50 -0300

    incluindo novos endpoints na secao 3

---
+++ famp/secao03/consulta.py
@@ -0,0 +1,14 @@
+import requests
+
+reqUrl = "http://localhost:8000/cursos"
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+}
+
+payload = ""
+
+response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+
+print(response.text)
--- famp/secao03/main.py
+++ famp/secao03/main.py
@@ -1,16 +1,20 @@
-from distutils.log import debug
-from imp import reload
+from typing import List, Optional
 from fastapi import FastAPI
+from fastapi import HTTPException, status
+
+from models import Curso
 
 app = FastAPI()
 
 cursos = {
     1: {
+        "id": 1,
         "titulo": 'titulo 1',
         "aulas": 112,
         "horas": 200
     },
     2: {
+        "id": 2,
         "titulo": 'titulo 2',
         "aulas": 224,
         "horas": 400
@@ -20,6 +24,32 @@ cursos = {
 @app.get('/cursos')
 async def get_cursos():
     return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail='Curso não encontrado')
+
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    curso.id = next_id
+    cursos[next_id] = curso
+    return curso
+
+@app.put('/cursos/{curso_id}', status_code=status.HTTP_200_OK)
+async def update_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f'Curso de id:{curso_id} não encontrado')
 
 
 
