d4dbcce9dab507d4be83ff57446f4bcb8fb30739
Alison Ghedin <alisonjghedin@gmail.com>
2022-06-17 16:32:46 -0300

    adicionado Path para get_curso

--- famp/secao03/main.py
+++ famp/secao03/main.py
@@ -1,7 +1,9 @@
 from typing import List, Optional
 from fastapi import FastAPI
 from fastapi import HTTPException, status
-
+from fastapi import Response
+from fastapi import Path
+# from fastapi.responses import JSONResponse
 from models import Curso
 
 app = FastAPI()
@@ -26,7 +28,10 @@ async def get_cursos():
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(default=None,
+                                        title='ID do curso', 
+                                        description='Deve ser valor entre 1 e 5',
+                                        ge=1, lt=6)):
     try:
         curso = cursos[curso_id]
         return curso
@@ -42,16 +47,28 @@ async def post_curso(curso: Curso):
     cursos[next_id] = curso
     return curso
 
-@app.put('/cursos/{curso_id}', status_code=status.HTTP_200_OK)
+@app.put('/cursos/{curso_id}')
 async def update_curso(curso_id: int, curso: Curso):
     if curso_id in cursos:
         cursos[curso_id] = curso
         return curso
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f'Curso de id:{curso_id} não encontrado')
+        detail=not_found(curso_id))
+
+@app.delete('/cursos/{curso_id}')
+async def destroy(curso_id: int):
+    try:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=not_found(curso_id))
 
 
+def not_found(id):
+    return 'Curso de id: {id} não encontrado'
 
 if __name__ == '__main__':
     import uvicorn
