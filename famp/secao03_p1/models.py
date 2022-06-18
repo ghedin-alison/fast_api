ca1e484f9912c047aab51ed05ee224fc44165bc9
Alison Ghedin <alisonjghedin@gmail.com>
2022-06-18 20:03:34 -0300

    include new validation on models

--- famp/secao03_p1/models.py
+++ famp/secao03_p1/models.py
@@ -10,8 +10,12 @@ class Curso(BaseModel):
     horas: int
 
     @validator('titulo')
-    def validate_titulo(cls, value):
+    def validate_titulo(cls, value: str):
         palavras = len(value.split(' '))
         if palavras < 3:
             raise ValueError('o Título deve ter pelo menos 3 palavras')
+
+        if value.islower():
+            raise ValueError('o Titulo deve ter Iniciais em maiusculas')
+
         return value
